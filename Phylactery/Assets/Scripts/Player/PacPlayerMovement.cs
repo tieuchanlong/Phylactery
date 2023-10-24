06790fc74dcfb2220494964ac06a4c88969fc9cc
Amy <96462599+amyyeeunchu@users.noreply.github.com>
2023-10-24 01:50:58 -0400

    Added player control for game jam#1
    basic keyboard controls to move the player left, right, up, and down
    (the main project will have similar structure with more actions provided)

---
+++ Phylactery/Assets/Scripts/Player/PacPlayerMovement.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5.0f;
+
+    private Rigidbody2D body;
+    private SceneHandler scene;
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+        // access component
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // press D to move right
+        if(Input.GetKeyDown(KeyCode.D)) {
+            MoveRight();
+        }
+      
+        // press W to move up
+        if(Input.GetKeyDown(KeyCode.W)) {
+            MoveUp();
+        }
+
+        // press A to move left
+        if(Input.GetKeyDown(KeyCode.A)) {
+            MoveLeft();
+
+        // press S to move left
+        if(Input.GetKeyDown(KeyCode.S)) {
+            MoveDown();
+        }        
+    }
+
+    // Move to the right every frame *per second*
+    void MoveRight() {   
+            transform.position +=
+                new Vector3(speed, 0.0f, 0.0f) * Time.deltaTime;
+    }
+
+    void MoveLeft() {   
+            transform.position +=
+                new Vector3(-speed, 0.0f, 0.0f) * Time.deltaTime;
+    }
+
+    void MoveUp() {   
+            transform.position +=
+                new Vector3(0.0f, speed, 0.0f) * Time.deltaTime;
+    }
+
+    void MoveDown() {   
+            transform.position +=
+                new Vector3(0.0f, -speed, 0.0f) * Time.deltaTime;
+    }
+}
